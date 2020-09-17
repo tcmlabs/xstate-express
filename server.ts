1d76dc1bd4534c6fc53a0f7b5921b00ac328684a
Jean-Baptiste Musso <jeanbaptiste@tcmlabs.fr>
2020-09-17 13:39:25 +0200

    wip

--- server.ts
+++ server.ts
@@ -1,45 +1,147 @@
 import express, { Request, Response, Router } from "express";
-import { Machine } from "xstate";
+import { interpret, Machine } from "xstate";
 
-const toggleMachine = Machine({
-  initial: "unlit",
-  states: {
-    unlit: {
-      on: {
-        TOGGLE: "lighted",
-        BREAK: "broken",
+const resolveToOK = () => {
+  console.log("async invoke???");
+  return Promise.resolve("ok");
+};
+
+const makeMachine = (initialState: string) => {
+  const toggleMachine = Machine({
+    initial: initialState,
+    states: {
+      checkingunlit: {
+        invoke: {
+          id: "toto",
+          src: "resolveToOK",
+          onDone: { target: "lit" },
+          onError: { target: "unlit" },
+        },
+      },
+      checkinglit: {
+        invoke: {
+          id: "tata",
+          src: "resolveToOK",
+          onDone: { target: "unlit" },
+          onError: { target: "lit" },
+        },
+      },
+      unlit: {
+        on: {
+          TOGGLE: "checkingunlit",
+          BREAK: "broken",
+        },
+      },
+      lit: {
+        on: {
+          TOGGLE: "checkinglit",
+          BREAK: "broken",
+        },
+      },
+      broken: {
+        type: "final",
       },
     },
-    lighted: {
-      on: {
-        TOGGLE: "unlit",
-        BREAK: "broken",
+  }).withConfig({
+    actions: {
+      //
+    },
+    services: {
+      resolveToOK: async (context, event) => {
+        console.log("  invoked", event.type);
+        return resolveToOK();
       },
     },
-    broken: {
-      type: "final",
-    },
-  },
-});
+  });
 
-const jsonState = JSON.stringify(toggleMachine);
+  return toggleMachine;
+};
 
-console.log(jsonState);
+let globalMachine = makeMachine("lit");
+let latestGlobalMachineState = globalMachine.initialState;
+
+const machineDescription = JSON.stringify(globalMachine);
+
+// console.log(machineDescription);
 
 const app = express();
 
-const nextEvents = toggleMachine.events;
+const nextEvents = globalMachine.events;
 
 const router = express.Router();
 
-let currentState = toggleMachine.initialState;
+const machineRouter = nextEvents.reduce((accRouter: Router, eventName) => {
+  accRouter.get(`/${eventName}`, async (_req: Request, res: Response) => {
+    let currentState = latestGlobalMachineState;
 
-const machineRouter = nextEvents.reduce((accRouter: Router, event) => {
-  accRouter.get(`/${event}`, (_req: Request, res: Response) => {
+    // const machine = makeMachine(currentState.value as string);
+    const httpMachine = makeMachine(currentState.value as string);
+
     // TODO: load state + context from database
-    currentState = toggleMachine.transition(currentState, event);
+    console.log("------");
+    console.log("current", currentState.value);
+    console.log("transition:", eventName);
+    // const currentState = currentState.value;
 
-    res.send(currentState.value);
+    const fromState = `${currentState.value}`;
+    // const viaEventName = `checking${currentState.value}`;
+    const viaEventName = eventName;
+
+    const currentStateValue = currentState.value;
+    console.log("currentState", currentStateValue);
+
+    const machineService = interpret(httpMachine)
+      .onTransition((newState) => {
+        console.log("state transition:", newState.value);
+
+        if (newState.value === currentState.value) {
+          console.log("Do nothing");
+          return;
+        }
+
+        if ((newState.value as string).startsWith("checking")) {
+          console.log("Checking...");
+
+          return;
+        }
+
+        console.log("Send updated state");
+        // if (newState.value === "unlit" && currentState.value === "lit") {
+        // console.log("...unlit");
+        // globalMachine = httpMachine;
+        console.log({ fromState, eventName: viaEventName });
+
+        console.log("=====");
+        // console.log(globalMachine);
+        const nextGlobalMachineState = globalMachine.transition(
+          fromState,
+          viaEventName
+        );
+        console.log("========");
+        console.log("========");
+        console.log("========");
+        // console.log(globalMachine);
+        latestGlobalMachineState = newState;
+        console.log(nextGlobalMachineState.value);
+        // // console.log(globalMachine.start)
+
+        res.send({ nextState: newState.value });
+        return;
+      })
+
+      .start();
+
+    machineService.send(viaEventName);
+
+    // machineService.subscribe((state) => {
+    //   console.log("subscribe", state.value);
+    // });
+
+    // console.log('here')
+    // await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+    // console.log(there)
+
+    // res.send({ nextState });
   });
   return accRouter;
 }, router);
